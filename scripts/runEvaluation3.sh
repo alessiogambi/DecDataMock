e5ec33cbc5e1bad400abeed0fffb3b0960e0920d
Alessio Gambi <alessio.gambi@usi.ch>
2014-09-02 09:58:44 +0200

    Update the Schedule.pbj object to run evaluation 3.2

--- scripts/runEvaluation3.sh
+++ scripts/runEvaluation3.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#         "testsuites/evaluation-3.xml" simultaneity
-#        "testsuites/evaluation-3.1.xml" automation
 
 export MAVEN_OPTS="-Xmx1024m"
 
@@ -8,14 +6,16 @@ OLD_DIR=`pwd`
 
 cd ..
 
+#        "testsuites/evaluation-3.1.xml" 
+#        "testsuites/evaluation-3.2.xml"
 # Apply the Patch to Schedule: cp SimultaneousSchedule -> Schedule.pbj and recompile.
 Echo "Apply the Schedule Patch for Simultaneous events"
 
 cp -v ./src//test/resources/at/ac/testing/mocks/Schedule.pbj.evaluation3 ./src//test/resources/at/ac/testing/mocks/Schedule.pbj
 
 for testSuite in \
-	"testsuites/evaluation-3.xml" \
-	"testsuites/evaluation-3.1.xml"
+	"testsuites/evaluation-3.1.xml" \
+	"testsuites/evaluation-3.2.xml"
 
 do
 	echo "Processing $testSuite"
@@ -24,7 +24,6 @@ do
 	# Prepare the Package to mv to output folder
         OUTPUT_DIR="reports/"`basename $testSuite .xml` 
         if [ -d "$OUTPUT_DIR" ]; then rm -rf $OUTPUT_DIR; fi
-	mkdir $OUTPUT_DIR
 
 	echo "Moving Surefire Report Folder to $OUTPUT_DIR"
 	mv -v ./target/surefire-reports $OUTPUT_DIR
